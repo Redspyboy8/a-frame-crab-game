f59eae101a5d87c00ab8d0af4d32b205722b30ba
lunchtray-chen <gtc477@gmail.com>
2025-11-18 18:26:12 -0800

    Added intro sequence
    - Pressing play will take player to intro story sequence
    - Text advances via clicking
    - Sequence can be skipped with skip button, takes player to game scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        print("hi");
+        SceneManager.LoadScene("IntroScene");
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SkipIntro()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
